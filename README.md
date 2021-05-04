e06beca749d63c53fd71187e65c26a7e0e948a4b
setapdede <83645578+setapdede@users.noreply.github.com>
2021-05-04 18:29:18 +0300

    Merge (#3)
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    Co-authored-by: pivinx1 <63850629+pivinx1@users.noreply.github.com>

--- README.md
+++ README.md
@@ -4,4 +4,7 @@ An CLI-based assistant made by pivinx1
 - 5 commands except exit
 - 4 greetings
 - 2 funny jokes :)
+
 - Response for when you didn't type a command correctly
+
+
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import datetime
 greetingID = random.randint(0, 4)
 usercmd = input("\nPlease input how do you want to be called\n")
 user = usercmd
+exit = 0
 # Greet the user
 print("\nDEBUG_GREET: ", greetingID)
 if greetingID == 0:
@@ -27,6 +28,84 @@ while True:
         print("\nYou're", user, "if I'm not mistaken. \n")
     elif usercmd == "Exit":
         print("\nBye", user,"!")
+        quit()
+    #Quick Menu
+    elif usercmd == "Show the Quick Menu":
+        print("\nQuick Menu\n")
+        print("DOS")
+        print("Progressbar95")
+        usercmd = input("Return to Sparrow\n")
+        if usercmd == "Return to Sparrow":
+          print("Returned")
+        if usercmd == "DOS":
+          exit = 0
+          print("\nDOS for Sparrow Assistant SD\n")
+          while exit == 0:
+           DOS = input(">")
+           if DOS == "exit":
+            exit = 1;
+        if usercmd == "Progressbar95":
+          progressbar = 0
+          while progressbar < 101:
+           segment = random.randint(0, 1)
+           progressbar2 = progressbar
+           if (progressbar == 100):
+             print("Congrats you won!")
+             progressbar == 102
+           if (segment == 0):
+             print("\nBlue")
+           if (segment == 1):
+             print("\nRed")
+           print("Progressbar is", progressbar,"% filled")
+           pb95 = input("\ntype c to catch the segment or type anything to avoid it\n")
+           if (pb95 == "c"):
+             if (segment == 0):
+               if (progressbar == 0):
+                 progressbar = 5
+               elif (progressbar == 5):
+                 progressbar = 10
+               elif (progressbar == 10):
+                 progressbar = 15
+               elif (progressbar == 15):
+                 progressbar = 20
+               elif (progressbar == 20):
+                 progressbar = 25
+               elif (progressbar == 25):
+                 progressbar = 30
+               elif (progressbar == 30):
+                 progressbar = 35
+               elif (progressbar == 35):
+                 progressbar = 40
+               elif (progressbar == 40):
+                 progressbar = 45
+               elif (progressbar == 45):
+                 progressbar = 50
+               elif (progressbar == 50):
+                 progressbar = 55
+               elif (progressbar == 55):
+                 progressbar = 60
+               elif (progressbar == 60):
+                 progressbar = 65
+               elif (progressbar == 65):
+                 progressbar = 70
+               elif (progressbar == 70):
+                 progressbar = 75
+               elif (progressbar == 75):
+                 progressbar = 80
+               elif (progressbar == 80):
+                 progressbar = 85
+               elif (progressbar == 85):
+                 progressbar = 90
+               elif (progressbar == 90):
+                 progressbar = 95
+               elif (progressbar == 95):
+                 progressbar = 100
+             if (segment == 1):
+              progressbar = 0
+             
+    #About
+    elif usercmd == "Who created you?":
+        print("\nI was originaly built by pivinx1, but this fork is made by my master setapdede.")
         quit()
     #Quick Menu
     elif usercmd == "Show the Quick Menu":
