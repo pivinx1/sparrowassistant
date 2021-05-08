7fba5024716a6656db05d3342fd2cdddda615452
BurningInfern0 <74492478+BurningInfern0@users.noreply.github.com>
2021-05-08 14:07:34 -0400

    ProgressCLI95 version 0.2
    Added:
    + Pink and Orange segment
    + Lives and Score system
    + A way to see your progressbar
    + Credits
    + A way to quit
    + Segment Art
    + Screen Clearing

--- progressbar.py
+++ progressbar.py
@@ -1,20 +1,115 @@
+# Imports
 import random
+from os import system, name
+from time import sleep
+
+def clear():
+    if name == "nt":
+        _ = system('cls')
+    else:
+        _ = system('clear')
+
 def progressbar():
     progressbar = 0
+    progressbar2 = 0
+    lives = 3
+    score = 0
+    # level = 1
+    bar = []
+    bardisplay = ""
+    segments = ""
     while progressbar != 100:
-        seg = random.randint(0, 1)
+        # clears the screen for next segment
+        clear()
+
+        # checks if lives are 0, breaks if true
+        if lives == 0:
+            print("You are out of lives. Game over!")
+            break
+
+        # randomly chooses a segment and displays its art
+        seg = random.randint(0, 3)
         if seg == 0:
-            seg_name = "blue"
+            seg_art = "╔══╗\n║  ║\n║  ║\n╚══╝"
+        elif seg == 1:
+            seg_art = "╔══╗\n║!!║\n║!!║\n╚══╝"
+        elif seg == 2:
+            seg_art = "╔══╗\n║--║\n║--║\n╚══╝"
         else:
-            seg_name = "red"
-        print("The segment falling is the", seg_name, "segment")
-        print("You have", progressbar, "%", "in your progressbar.")
-        catch = input("Type in c to catch, anything else to shy away\n")
+            seg_art = "╔══╗\n║~~║\n║~~║\n╚══╝"
+        print(seg_art)
+
+        # checks if you have 1 life left
+        if lives == 1:
+            print("You have 1 life left. Be careful.")
+        else:
+            print("You have", lives, "lives left.")
+
+        # checks if you have orange segments in your bar
+        bardisplay = ""
+        if progressbar2 > 0:
+            for segments in bar:
+                bardisplay = bardisplay + segments + ", "
+            print("\nYour bar:", bardisplay, "\n")
+            print("You have", progressbar, "% with", progressbar2, "% orange in your progressbar.")
+        else:
+            for segments in bar:
+                bardisplay = bardisplay + segments + ", "
+            print("\nYour bar:", bardisplay, "\n")
+            print("You have", progressbar,"%", "in your progressbar.")
+
+        # catches the currently displayed segment
+        catch = input("Type 'C' to catch, any other key to move away, and 'Q' to quit.\n")
+
+        # calculates which segment you caught and does stuff
         if seg == 0 and catch == "c":
             progressbar = progressbar + 5
+            bar.append("Blue")
+            score = score + 5
         elif seg == 1 and catch == "c":
+            bar = []
+            bardisplay = ""
+            lives = lives - 1
             progressbar = 0
+            progressbar2 = 0
+            score = score - 10
+        elif seg == 2 and catch == "c":
+            if progressbar == 0:
+                continue
+            if bar[-1] == "Orange":
+                progressbar2 = progressbar2 - 5
+                progressbar = progressbar - 5
+                bar.pop(-1)
+                score = score + 5
+            else:
+                progressbar = progressbar - 5
+                bar.pop(-1)
+                score + score - 5
+        elif seg == 3 and catch == "c":
+            progressbar = progressbar + 5
+            progressbar2 = progressbar2 + 5
+            bar.append("Orange")
+
+        if catch == "q":
+            print('Game Over! Thanks for playing!')
+            break
+
+        if catch == "credits":
+            clear()
+            print('ProgressCLI95 0.2')
+            print('Original code (0.1) by Setapdede')
+            print('Improved code (0.2) by BurningInfern0')
+            print('Made for use with Sparrow Assistant by pivinx1')
+            print('\nPress ENTER to get back to the game.')
+            input()
+
+        # if you have 100% on your progressbar, the game will end.
+        if progressbar == 100:
+            if progressbar2 > 0:
+                print('Bravo!')
+            else:
+                print('Perfect!')
+            print('Good game! Your final score is:', score)
+            break
         else:
             continue
-        if progressbar == 100:
-            print("Perfect!")
