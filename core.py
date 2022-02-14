5e5a4a26463da08914b5654e61808c2876df27db
pivinx1 <pivinx1@gmail.com>
2022-02-14 19:26:57 +0100

    Add a greeting

--- core.py
+++ core.py
@@ -1,2 +1,14 @@
 # Project Sparrow
 # CopyLEFT pivinx1 2021-2022
+# How modular it can get? Let's find out!
+# Go on, break my muppet.
+# This bad boy, or girl, is supposed to be extremely modular. Expect this to be FILLED with module calls.
+import userwrapper as user
+
+username = user.userwrapper()
+user.greet(username)
+
+while True:
+    print("This section is under construction. Wait, why did pivinx1 even create this loop? Anyways, get out.")
+    # To-do: Remove break once there is something actually useful in this loop.
+    break
---
+++ userwrapper.py
@@ -0,0 +1,14 @@
+# Userwrapper
+# Effectively does things like pull username.
+import time
+
+
+def userwrapper():
+    print("Sparrow Assistant version 0.2.0 - Under Construction")
+    username = input("Username:")
+    # You shouldn't expect more from this.
+    return username
+def greet(username):
+    datetime = time.time()
+    current_time = time.ctime(datetime)
+    print("Welcome,", username + ". The time is", current_time + ".")
