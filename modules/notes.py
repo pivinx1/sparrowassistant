23690b90c23f1b35a2edda324c250fed5095813e
pivinx1 <pivinx1@gmail.com>
2022-02-17 14:07:22 +0100

    Added some comments to notes.py
    (Even though it's a relatively small file.)

--- modules/notes.py
+++ modules/notes.py
@@ -1,5 +1,6 @@
 # To-do: make a delete note and read note function, unless you wanna do these two things by yourself, then there is nothing stopping ya :v
 def takeNote(text):
+    # Open the note file (or make a new one); then append data passed through the text variable to the note.
     note = open("note.snf", "a")
     note.write(text)
     note.close()
