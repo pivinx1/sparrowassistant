68ee1bed5fa48c573617335d032aabc31c36ab00
pivinx1 <pivinx1@gmail.com>
2022-02-17 13:59:10 +0100

    Update notes.py

--- modules/notes.py
+++ modules/notes.py
@@ -1,4 +1,4 @@
-
+# To-do: make a delete note and read note function, unless you wanna do these two things by yourself, then there is nothing stopping ya :v
 def takeNote(text):
     note = open("note.snf", "a")
     note.write(text)
