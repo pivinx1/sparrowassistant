a7c02776ca2d6fb7e7d49fdc8570cb6f67b513e8
pivinx1 <pivinx1@gmail.com>
2022-02-25 09:35:18 +0100

    Improved notes feature

--- core.py
+++ core.py
@@ -13,10 +13,21 @@ user.greet(username)
 while True:
     # Off to making a huge disaster of an if-elif chain. Now split into menus.
     command = input(">")
-    if command == "menu":
+    if command == "Menu":
         print("Menu\nRetype the menu entry name to the prompt to run the command.\n1. Notepad\n2. ProgressCLI 95")
     elif command == "Notepad":
-        noteText = input("Type in what do you want to note.\n")
-        notes.takeNote(noteText)
+        print("Notepad\nRetype menu entry name to run the command.\n1. Add new line to note\n2. Delete note\n3. Read note\n4. Exit")
+        noteCommand = input(">")
+        if noteCommand == "Add new line to note":
+            noteText = input("Type in what do you want to note.\n")
+            notes.takeNote(noteText)
+        elif noteCommand == "Delete note":
+            notes.rmNote()
+        elif noteCommand == "Read note":
+            notes.readNote()
+        elif noteCommand == "Exit":
+            continue
+        else:
+            print("No such command")
     elif command == "ProgressCLI 95":
         remoteBoot()
--- modules/notes.py
+++ modules/notes.py
@@ -1,6 +1,17 @@
-# To-do: make a delete note and read note function, unless you wanna do these two things by yourself, then there is nothing stopping ya :v
+import os
+# The syntax highlighting got a little bit weird. Send help.
 def takeNote(text):
     # Open the note file (or make a new one); then append data passed through the text variable to the note.
     note = open("note.snf", "a")
-    note.write(text)
+    note.write(text + "\n")
+    note.close()
+def rmNote():
+    if os.path.exists("note.snf"):
+        os.remove("note.snf")
+        print("Done.")
+    else:
+        print("I couldn't delete your note, because there isn't one.")
+def readNote():
+    note = open("note.snf", "r")
+    print(note.read())
     note.close()
